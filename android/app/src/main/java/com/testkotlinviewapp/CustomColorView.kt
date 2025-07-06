27e86efdab565143fe13ff3d0196cae1b501eb58
aditya bihari dash <adash2022@gift.edu.in>
2025-07-06 16:36:56 +0530

    updated the file

--- android/app/src/main/java/com/testkotlinviewapp/CustomColorView.kt
+++ android/app/src/main/java/com/testkotlinviewapp/CustomColorView.kt
@@ -3,32 +3,63 @@ package com.testkotlinviewapp
 import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
-import android.util.AttributeSet
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class CustomColorGLView(context: Context) : GLSurfaceView(context) {
 
-    private val renderer: ColorRenderer
+    private val renderer: ColorTriangleRenderer
 
     init {
         setEGLContextClientVersion(2)
-        renderer = ColorRenderer()
+        renderer = ColorTriangleRenderer()
         setRenderer(renderer)
         renderMode = RENDERMODE_WHEN_DIRTY
     }
 
     fun setColor(colorString: String) {
-        renderer.setColor(colorString)
+        renderer.setBackgroundColor(colorString)
         requestRender()
     }
 
-    private class ColorRenderer : Renderer {
+    private class ColorTriangleRenderer : Renderer {
+
         private var red = 0f
         private var green = 1f
         private var blue = 0f
 
-        fun setColor(color: String) {
+        private lateinit var vertexBuffer: FloatBuffer
+        private val triangleCoords = floatArrayOf(
+            0.0f, 0.5f, 0.0f,  // top
+           -0.5f, -0.5f, 0.0f, // bottom left
+            0.5f, -0.5f, 0.0f  // bottom right
+        )
+
+        private val vertexShaderCode = """
+            attribute vec4 vPosition;
+            void main() {
+                gl_Position = vPosition;
+            }
+        """
+
+        private val fragmentShaderCode = """
+            precision mediump float;
+            uniform vec4 vColor;
+            void main() {
+                gl_FragColor = vColor;
+            }
+        """
+
+        private var program = 0
+        private var positionHandle = 0
+        private var colorHandle = 0
+
+        private val triangleColor = floatArrayOf(1f, 1f, 1f, 1f) // White
+
+        fun setBackgroundColor(color: String) {
             try {
                 val parsed = android.graphics.Color.parseColor(color)
                 red = (parsed shr 16 and 0xFF) / 255f
@@ -39,17 +70,51 @@ class CustomColorGLView(context: Context) : GLSurfaceView(context) {
             }
         }
 
-        override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-            GLES20.glClearColor(red, green, blue, 1f)
+        override fun onSurfaceCreated(unused: GL10?, config: EGLConfig?) {
+            GLES20.glClearColor(red, green, blue, 1.0f)
+
+            val bb = ByteBuffer.allocateDirect(triangleCoords.size * 4)
+            bb.order(ByteOrder.nativeOrder())
+            vertexBuffer = bb.asFloatBuffer()
+            vertexBuffer.put(triangleCoords)
+            vertexBuffer.position(0)
+
+            val vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+            val fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+            program = GLES20.glCreateProgram().also {
+                GLES20.glAttachShader(it, vertexShader)
+                GLES20.glAttachShader(it, fragmentShader)
+                GLES20.glLinkProgram(it)
+            }
         }
 
-        override fun onDrawFrame(gl: GL10?) {
+        override fun onDrawFrame(unused: GL10?) {
             GLES20.glClearColor(red, green, blue, 1f)
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+            GLES20.glUseProgram(program)
+
+            positionHandle = GLES20.glGetAttribLocation(program, "vPosition")
+            GLES20.glEnableVertexAttribArray(positionHandle)
+            GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer)
+
+            colorHandle = GLES20.glGetUniformLocation(program, "vColor")
+            GLES20.glUniform4fv(colorHandle, 1, triangleColor, 0)
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3)
+            GLES20.glDisableVertexAttribArray(positionHandle)
         }
 
-        override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        override fun onSurfaceChanged(unused: GL10?, width: Int, height: Int) {
             GLES20.glViewport(0, 0, width, height)
+        }
+
+        private fun loadShader(type: Int, shaderCode: String): Int {
+            return GLES20.glCreateShader(type).also { shader ->
+                GLES20.glShaderSource(shader, shaderCode)
+                GLES20.glCompileShader(shader)
+            }
         }
     }
 }
